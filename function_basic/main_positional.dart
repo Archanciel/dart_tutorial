0311e6afcbf6e29fe1452717b433f13b44294dd2
Archanciel <jp.schnyder@gmail.com>
2022-02-24 17:41:53 +0100

    Update main_positional.dart

--- function_basic/main_positional.dart
+++ function_basic/main_positional.dart
@@ -12,6 +12,6 @@ String describePositionalNullableArgument(String? name, int? age) {
   return 'DPNA: I am ${name ?? 'No Name'} born in ${2022 - (age ?? 0)}';
 }
 
-String describePositionalDefaultArgument(String name = '', int age = 0) {
+String describePositionalDefaultArgument([String name = '', int age = 0]) {
   return 'DPDA: I am $name born in ${2022 - age}';
 }
