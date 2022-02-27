bdde6a9fa9b9600929dc8794c0e95cfed303972c
Archanciel <jp.schnyder@gmail.com>
2022-02-27 21:13:15 +0100

    Update generic_func.dart

--- function_basic/generic_func.dart
+++ function_basic/generic_func.dart
@@ -2,9 +2,12 @@ void main() {
   List<int> lstInt = [1, 2, 3];
   List<double> lstDouble = [11.65, 2.74, 3.897646];
 
-  print('computeLstGeneric int square: ${computeLstGeneric<int>(lstInt, (int val) => val * val)}');
-  print('computeLstGeneric double cube: ${computeLstGeneric<double>(lstDouble, (double val) => val * val * val)}');
-  print('computeLstGenericReturn double cube rounded to int: ${computeLstGenericReturn<double, int>(lstDouble, (double val) => (val * val * val).round())}');
+  print('''computeLstGeneric int square: 
+    ${computeLstGeneric<int>(lstInt, (int val) => val * val)}''');
+  print('''computeLstGeneric double cube: 
+    ${computeLstGeneric<double>(lstDouble, (double val) => val * val * val)}''');
+  print('''computeLstGenericReturn double cube rounded to int: 
+    ${computeLstGenericReturn<double, int>(lstDouble, (double val) => (val * val * val).round())}''');
 }
 
 List<T> computeLstGeneric<T>(List<T> lst, T Function(T) f) {
