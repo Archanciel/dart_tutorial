453111d841c024b1abb936cfe039a62fc043ad83
Archanciel <jp.schnyder@gmail.com>
2022-03-08 22:53:58 +0100

    Create def_value_op.dart

---
+++ revision/def_value_op.dart
@@ -0,0 +1,25 @@
+void main() {
+  /*
+   * Using ??= named default value operator or null aware operator.
+   */
+
+  double? dn;
+  print(dn?.round()); // null
+  double e = dn ??= 100.500001;
+  print(dn); // 100.500001
+  print(e); // 100.500001
+
+  Map<String, double> mp = {'one': 1.0, 'two': 2.0};              
+  double f = mp['boo'] ??= 4.0;
+  print(mp['boo']); // 4
+  print(f); // 4
+
+  double? dnn;
+  dnn ??= 5.78;
+  print(dnn); // 5.78
+
+  double? dnnn = 55.01;
+  //dnnn = null; // if executed, then on next line dnnn == 55.32
+  dnnn ??= 55.32;
+  print(dnnn); // 55.01. Since dnnn was not null, its value was not changed by ??=  
+}
