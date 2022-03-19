3c254af4bea08357af8a7156d47ac9cc4b2c7842
Archanciel <jp.schnyder@gmail.com>
2022-03-19 03:15:10 +0100

    learning

---
+++ class_basic/class_advanced/operator_exercise.dart
@@ -0,0 +1,38 @@
+class Point {
+  int _x;
+  int _y;
+
+  Point({required int x, required int y})
+      : _x = x,
+        _y = y;
+
+  int get x => _x;
+  set x(int x) => _x = x;
+
+  int get y => _y;
+  set y(int y) => _y = y;
+
+  // covariant not required here since + operator not implemented in Object
+  // class
+  Point operator +(Point point) => Point(x: _x + point.x, y: _y + point.y);
+
+  Point operator *(int val) => Point(x: _x * val, y: _y * val);
+
+  String desc() => 'x: $_x, y: $_y';
+
+  @override
+  // TODO: implement hashCode
+  int get hashCode => super.hashCode;
+}
+
+void main() {
+  Point p1 = Point(x: 2, y: 5);
+  Point p2 = Point(x: 3, y: 2);
+
+  print(p1.desc()); // x: 2, y: 5
+
+  print((p1 + p2).desc()); // x: 5, y: 7
+  print((p1 * 5).desc()); // x: 10, y: 25
+
+  print(p1.desc()); // x: 2, y: 5
+}
