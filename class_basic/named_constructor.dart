094ec2eb13e8122c48355e703f2e5b334404a718
Archanciel <jp.schnyder@gmail.com>
2022-03-20 11:40:36 +0100

    renamed file

--- class_basic/named_constructor.dart
+++ class_basic/named_constructor.dart
@@ -1,4 +1,5 @@
 class TemperatureNoGetterSetter {
+  // Named constructor class
   double celsius;
   double fahrenheit;
 
@@ -13,6 +14,7 @@ class TemperatureNoGetterSetter {
 }
 
 class Temperature {
+  // Named constructor class
   double celsius;
 
   Temperature.celsius(this.celsius);
@@ -42,29 +44,40 @@ void main() {
   TemperatureNoGetterSetter tc_no_gs = TemperatureNoGetterSetter.celsius(20);
   TemperatureNoGetterSetter tf_no_gs = TemperatureNoGetterSetter.fahrenheit(68);
 
-  print(tc_no_gs.description());
-  print(tf_no_gs.description());
+  print(tc_no_gs.description()); // celsius: 20.0 = fahrenheit 68.0
+  print(tf_no_gs.description()); // celsius: 20.0 = fahrenheit 68.0
 
   Temperature tc = Temperature.celsius(20);
   Temperature tf = Temperature.fahrenheit(68);
 
   print('\ntc.description');
-  print(tc.description());
+  print(tc.description()); // tc.description
+//                            fahrenheit getter called
+//                            celsius: 20.0 = fahrenheit 68.0
 
   print('\ntf.description');
-  print(tf.description());
+  print(tf.description()); // tf.description
+//                            fahrenheit getter called
+//                            celsius: 20.0 = fahrenheit 68.0
+
 
   print('\ntc.celsius');
-  print('tc.celsius ${tc.celsius}');
+  print('tc.celsius ${tc.celsius}'); // tc.celsius
+//                                      tc.celsius 20.0
 
   print('\ntc.fahrenheit');
-  print('tc.fahrenheit ${tc.fahrenheit}');
+  print('tc.fahrenheit ${tc.fahrenheit}'); // tc.fahrenheit
+//                                            fahrenheit getter called
+//                                            tc.fahrenheit 68.0
+//                                            20.0
 
   Temperature tc_2 = Temperature.celsius(20);
   print(tc_2.celsius);
 
   print('\ntc_2.fahrenheit = 100');
   tc_2.fahrenheit = 100;
-  print(tc_2.celsius);
+  print(tc_2.celsius); // tc_2.fahrenheit = 100
+//                        fahrenheit setter called
+//                        37.77777777777778
 }
 
