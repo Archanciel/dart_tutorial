ef875e719629f8de0a14b52423d2a10222fd2ae2
Archanciel <jp.schnyder@gmail.com>
2022-03-21 02:21:03 +0100

    Create covariant.dart

---
+++ class_basic/class_advanced/covariant.dart
@@ -0,0 +1,29 @@
+class Point {
+  int _x;
+  int _y;
+
+  Point({required int x, required int y})
+      : _x = x,
+        _y = y;
+
+  int get x => _x;
+  set x(int x) => _x = x;
+
+  int get y => _y;
+  set y(int y) => _y = y;
+
+  bool operator ==(covariant Point obj) {
+
+    return _x == obj.x && _y == obj.y;
+  }
+}
+
+void main() {
+  Point p1 = Point(x: 2, y: 5);
+  Point p2 = Point(x: 3, y: 2);
+  Point p3 = Point(x: 2, y: 5);
+
+  print(p1 == p2); // false
+  print(p1 == p3); // true
+  print(p1 == 'abc'); // false
+}
