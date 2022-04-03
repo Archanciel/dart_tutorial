2be606008ed546f9c9cc23e9eaddb4dc6d565698
Archanciel <jp.schnyder@gmail.com>
2022-04-03 18:29:06 +0200

    Update stream_delayed_explore.dart

--- asynchronous/stream_delayed_explore.dart
+++ asynchronous/stream_delayed_explore.dart
@@ -1,4 +1,6 @@
 Stream<T> delayedFromIterable<T>(Iterable<T> iter, int sec) async* {
+  // Returns a Stream in which element in the passed Iterable
+  // is delayed of sec seconds.
   for (T val in iter) {
     await Future.delayed(Duration(seconds: sec));
     yield val;
@@ -9,33 +11,40 @@ Future<void> main() async {
   print('main fromIterable');
 
   List<int> intLst = [1, 2, 3, 4, 5, 6, 7];
-  Stream<int> delayedIntStream = Stream<int>.fromIterable(intLst);
 
+  // using the Stream fromIterable() named constructor does not enable
+  // to add delays on the stream elements
+  Stream<int> intStream = Stream<int>.fromIterable(intLst);
+
+  // expression below extract a sub stream from the intStream
   Stream<int> selectionSubStream =
-      delayedIntStream.map((event) => event).where((event) => event > 3);
+      intStream.map((event) => event).where((event) => event > 3);
 
   await selectionSubStream.forEach((element) async => print(element));
 
   print('main delayedFromIterable');
 
-  delayedIntStream = delayedFromIterable(intLst, 2);
+  Stream<int> delayedIntStream = delayedFromIterable(intLst, 2);
 
-  selectionSubStream = delayedIntStream.map((event) => event).where((event) => event > 3);
+  // expression below extract a sub stream from the delayedIntStream
+  selectionSubStream =
+      delayedIntStream.map((event) => event).where((event) => event > 3);
 
   DateTime before = DateTime.now();
 
-  await selectionSubStream.forEach((element) async {
-    print(element);
-  });
+  // although we are getting less elements than the initial delayedIntStream,
+  // walking through the selectionSubStream takes the same time than walking
+  // through the initial delayedIntStream !
+  await selectionSubStream.forEach(print);
 
   int df = DateTime.now().difference(before).inSeconds;
   print('duration $df seconds');
 
-  Stream<int> waitStr = delayedFromIterable(intLst, 2);
+  delayedIntStream = delayedFromIterable(intLst, 2);
 
   before = DateTime.now();
 
-  await waitStr.forEach(print);
+  await delayedIntStream.forEach(print);
 
   df = DateTime.now().difference(before).inSeconds;
   print('duration $df seconds');
