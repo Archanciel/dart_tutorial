83137502b950231f26d18125addab4d924732de8
Archanciel <jp.schnyder@gmail.com>
2022-04-03 02:39:43 +0200

    learn

--- asynchronous/stream_random.dart
+++ asynchronous/stream_random.dart
@@ -1,24 +1,28 @@
 import 'dart:math';
 
-Stream<int> getRandomValues(int streamLength) async* {
+Stream<int> getRandomDelays(int streamLength) async* {
   Random randomSecond = Random();
   int i = 0;
 
   while (i < streamLength) {
-    int nextInt = randomSecond.nextInt(5);
-    await Future.delayed(Duration(seconds: nextInt), (() => i++));
-    yield nextInt;
+    int nextSecDelay = randomSecond.nextInt(5);
+    await Future.delayed(Duration(seconds: nextSecDelay), (() => i++));
+    yield nextSecDelay;
   }
 }
 
 void main() {
   print('main start');
 
-  Stream<int> streamInt = getRandomValues(10);
+  Stream<int> streamSecDelays = getRandomDelays(10);
 
-  print('stream created');
+  print('stream of delays created');
 
-  streamInt.listen((event) => print('$event seconds'));
+  int i = 1;
 
-  print('nain end');
+  streamSecDelays.listen((event) {
+    print('${i++}: printed after $event seconds');
+  });
+
+  print('nain end printed before obtaining streamed delays');
 }
