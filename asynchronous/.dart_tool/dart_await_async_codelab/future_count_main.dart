d702c58d1c9dcc07697c1f60d0d805e0f85f59a9
Archanciel <jp.schnyder@gmail.com>
2022-05-29 02:09:40 +0200

    improved

--- asynchronous/.dart_tool/dart_await_async_codelab/future_count_main.dart
+++ asynchronous/.dart_tool/dart_await_async_codelab/future_count_main.dart
@@ -1,6 +1,6 @@
 Future<void> printOrderMessage() async {
   var order = await fetchUserOrder();
-  print('Awaiting user order...');
+  print('Awaited user order for 4 seconds ...');
   print('Your order is: $order');
 }
 
@@ -9,11 +9,13 @@ Future<String> fetchUserOrder() {
 }
 
 void main() async {
-//  countSeconds(5);
-
+  countSeconds(5);
+  print('countSeconds(5) was called and now the 5 created Future\'s are executing ...');
+/*
   for (int i = 0; i <= 5; i++) {
     await Future.delayed(Duration(seconds: 1), () => print(i));
   }
+*/
   await printOrderMessage(); // msg will be printed after 4 seconds
 }
 
@@ -21,7 +23,7 @@ void countSeconds(int s) {
   for (var i = 1; i <= s; i++) {
     // creates 4 instances of Future with the 4 Duration's of 1, 2, 3 and 4
     // seconds
-    var f = Future.delayed(Duration(seconds: i), () => print(i));
-    // print(identityHashCode(f));
+    Future<void> f = Future.delayed(Duration(seconds: i), () => print(i));
+    print('Future $i identityHashCode ${identityHashCode(f)}');
   }
 }
--- class_basic/class_ctor.dart
+++ class_basic/class_ctor.dart
@@ -22,15 +22,29 @@ class BankAccountMostUsedConstructor {
   }
 }
 
+class BankAccountMostUsedConstructorSubClass
+    extends BankAccountMostUsedConstructor {
+  final String secondHolder;
+
+  BankAccountMostUsedConstructorSubClass(
+      {required String super.holder,
+      double super.balance = 0,
+      required String this.secondHolder});
+
+  String description() {
+    return super.description() + ' ' + secondHolder;
+  }
+}
+
 void main() {
   BankAccountVerboseConstructor accountVerbose =
       BankAccountVerboseConstructor(holder: 'JPS', balance: 12000);
-
   print(accountVerbose.description()); // JPS 12000.0
 
   BankAccountMostUsedConstructor account =
       BankAccountMostUsedConstructor(holder: 'JPS', balance: 500);
-
   print(account.description()); // JPS 500.0
-}
 
+  BankAccountMostUsedConstructorSubClass accountTwoHolders =BankAccountMostUsedConstructorSubClass(holder: 'JPS', secondHolder: 'Béa');
+  print(accountTwoHolders.description()); // JPS 0.0 Béa 
+}
